c39d3c0eea484ec32b6488b3833a45e087d98dbd
Serena Serafini <xxladantessaxx@gmail.com>
2025-11-11 09:48:54 +0100

    Aggiunto main menu prima di iniziare il gioco

---
+++ hitomi_game/src/scenes/main_menu.js
@@ -0,0 +1,44 @@
+function preload(s) {
+
+}
+
+function create(s) {
+
+    // Questa scena contiene solamente il testo centrato.
+
+    PP.shapes.text_styled_add(s, 
+                PP.game.config.canvas_width / 2,
+                PP.game.config.canvas_height / 2,
+                "Hitomi",
+                100,
+                "Helvetica",
+                "normal",
+                "0xFFFFFF",
+                null,
+                0.5,
+                0.5);
+
+  PP.shapes.text_styled_add(s, 
+                PP.game.config.canvas_width / 2,
+                PP.game.config.canvas_height / 5 * 4,
+                "Press Spacebar to Begin",
+                50,
+                "Helvetica",
+                "normal",
+                "0xFFFFFF",
+                null,
+                0.5,
+                0.5);
+}
+
+function update(s) {
+  if(PP.interactive.kb.is_key_down(s, PP.key_codes.SPACE)) {
+    PP.scenes.start("tutorial_scene");
+  }
+}
+
+function destroy(s) {
+
+}
+
+PP.scenes.add("main_menu", preload, create, update, destroy);
