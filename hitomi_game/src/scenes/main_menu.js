c65a88defc7df3fb62b961c789d1dade16f32d06
Serena Serafini <xxladantessaxx@gmail.com>
2025-11-11 10:10:16 +0100

    Modificato il main menu con titolo corretto

--- hitomi_game/src/scenes/main_menu.js
+++ hitomi_game/src/scenes/main_menu.js
@@ -4,13 +4,12 @@ function preload(s) {
 
 function create(s) {
 
-    // Questa scena contiene solamente il testo centrato.
+    // PP.game_state.set_variable("coins,0")
 
     PP.shapes.text_styled_add(s, 
                 PP.game.config.canvas_width / 2,
                 PP.game.config.canvas_height / 2,
-                "Hitomi",
-                100,
+                "Hitomi\nThrough The Other Side",100,
                 "Helvetica",
                 "normal",
                 "0xFFFFFF",
@@ -35,6 +34,9 @@ function update(s) {
   if(PP.interactive.kb.is_key_down(s, PP.key_codes.SPACE)) {
     PP.scenes.start("tutorial_scene");
   }
+  //if(PP.interactive.kb.is_key_down(s, PP.key_codes.SPACE)) {
+  //  PP.scenes.start("tutorial_scene");
+  //}
 }
 
 function destroy(s) {
