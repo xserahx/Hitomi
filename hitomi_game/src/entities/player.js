b5454479313acee161b3a48953edd5fd5c6cf14a
Fede-Lopre <federico1.lopresti@mail.polimi.it>
2025-12-05 14:12:23 +0100

    Bugfix: quando cambia mondo, la posizione del player è precisa

--- hitomi_game/src/entities/player.js
+++ hitomi_game/src/entities/player.js
@@ -200,10 +200,12 @@ PP.entities.player.get_baby = function (scene, player) {
   player.has_baby = true;
   console.log("Player has baby:", player.has_baby);
 }
+
+// === CAMBIO MONDO ===
 PP.entities.player.changeWorld = function (scene) {
   console.log("World changing to:", PP.game_state.otherWorld);
-  config.player_x = PP.game_state.player.geometry.body_x;
-  config.player_y = PP.game_state.player.geometry.body_y;
+  config.player_x = 40 + PP.game_state.player.geometry.body_x;
+  config.player_y = 60 + PP.game_state.player.geometry.body_y;
   PP.scenes.start(PP.game_state.otherWorld);
   PP.game_state.changingWorld = true;
 }
