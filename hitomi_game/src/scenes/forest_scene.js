ae6328fd8556bd2852b117d9db567038300a9961
Serena Serafini <xxladantessaxx@gmail.com>
2025-12-08 01:10:45 +0100

    Inizio inserimento assets foresta

--- hitomi_game/src/scenes/forest_scene.js
+++ hitomi_game/src/scenes/forest_scene.js
@@ -1,11 +1,14 @@
 // === FOREST SCENE ===
+let forest_bg;
 function preload_forest(scene) {
+  scene.load.image('snowflake', 'assets/images/forest/neve.png');
+  forest_bg = PP.assets.image.load(scene, "assets/images/forest/forest_background.png",1280,720);
+  PP.scene_objects.platform.preload(scene);
   PP.entities.player.preload(scene);
-
-  // Carica eventuali risorse future (audio, immagini, ecc.)
 }
 
 function create_forest(scene, data) {
+  PP.assets.tilesprite.add(scene, forest_bg, -20, 160, 1280, 720, 0, 0);
   // Setta la scena del mondo spettrale
     PP.game_state.otherWorld = "ghostly_forest_scene";
 
@@ -18,59 +21,51 @@ function create_forest(scene, data) {
   // === SFONDO ===
   scene.cameras.main.setBackgroundColor(0x0b3d0b);
 
-  // === LUCE / NEBBIA SOFT ===
- //  const overlay = scene.add.rectangle(1000, 300, 2000, 600, 0x00ff00); da cambiare con quello sotto
- // const overlay = scene.add.rectangle(1000, 300, 2000, 6000, 0x00ff00);
- // overlay.setAlpha(0.05);
- // overlay.setBlendMode(Phaser.BlendModes.ADD);
-
-// == NEVE ==
-
 // === GROUND ===
-    const ground = PP.shapes.rectangle_add(scene, 3200, 2000, 6400, 40, "0x4a3b2a", 1);
+    const ground = PP.shapes.rectangle_add(scene, 3100, 895, 6400, 40, "0x4a3b2a", 1);
     PP.physics.add(scene, ground, PP.physics.type.STATIC);
 
   // === PIATTAFORME "TRONCHI" ===
   const platformPositions = [
 
     // -- PRIMA CHAMBER --
-    { x: 320, y: 1850, w: 200, h: 20  },
-    { x: 740, y: 1850, w: 200, h: 20  },
-    { x: 970, y: 1720, w: 150, h: 20  },
+   // { x: 320, y: 1850, w: 200, h: 20  },
+  //  { x: 740, y: 1850, w: 200, h: 20  },
+   // { x: 970, y: 1720, w: 150, h: 20  },
     // { x: 1270, y: 1740, w: 150, h: 20 },
 
     // MASSI
-    { x: 1600, y: 1885, w: 150, h: 190},
-    { x: 1732, y: 1955, w: 100, h: 50 },
+   // { x: 1600, y: 1885, w: 150, h: 190},
+   // { x: 1732, y: 1955, w: 100, h: 50 },
 
     // SCALA
-    { x: 2080, y: 1850, w: 100, h: 20 },
-    { x: 2250, y: 1740, w: 100, h: 20 },
+    //{ x: 2080, y: 1850, w: 100, h: 20 },
+   // { x: 2250, y: 1740, w: 100, h: 20 },
 
     // BLOCCO A SINISTRA DELLA SCALA
    //   { x: 2050, y: 20 + 1300, w: 300, h: 20 },
 
     // SECONDA RAMPA DI SCALE 
-    { x: 2450, y: 1150, w: 100, h: 20 },
-    { x: 2675, y: 1070, w: 150, h: 20 },
-    { x: 3000, y: 1000, w: 150, h: 20 },  // apice
+   // { x: 2450, y: 1150, w: 100, h: 20 },
+   // { x: 2675, y: 1070, w: 150, h: 20 },
+   // { x: 3000, y: 1000, w: 150, h: 20 },  // apice
 
     // MASSI
-    { x: 2890, y: 1905, w: 200, h: 150},
+    //{ x: 2890, y: 1905, w: 200, h: 150},
 
     // PIATTAFROME FRA I DUE MASSI
-    { x: 3190, y: 1750, w: 100, h: 20 },
+   // { x: 3190, y: 1750, w: 100, h: 20 },
 
     //PIATTAFROME ADIACENTE ALL'ASCENSORE
-    { x: 3500, y: 1700, w: 100, h: 20 },
+    //{ x: 3500, y: 1700, w: 100, h: 20 },
 
     //PIATTAFORMA SOPRA L'ASCENSORE
-    { x: 3450, y: 1500, w: 200, h: 20 },
+    //{ x: 3450, y: 1500, w: 200, h: 20 },
 
     // SPAZIO PER MOVING PLATFORM
 
     // MASSI finale
-    { x: 3850, y: 1943, w: 160, h: 75 },
+    //{ x: 3850, y: 1943, w: 160, h: 75 },
   ];
 
   PP.game_state.platforms = PP.scene_objects.platform.create(scene, platformPositions);
@@ -86,7 +81,7 @@ function create_forest(scene, data) {
     let startX = scene.scene.settings.data?.x ?? PP.game_state.playerPosition?.x ?? 150;
     let startY = scene.scene.settings.data?.y ?? PP.game_state.playerPosition?.y ?? 500;
 
-    //Check se sta cambaindo mondo
+    //Check se sta cambiando mondo
     if(PP.game_state.changingWorld){
         startX = config.player_x;
         startY = config.player_y;
@@ -129,34 +124,27 @@ function create_forest(scene, data) {
   scene.cameras.main.setBounds(leftWall.geometry.body_x, 0, worldWidth, worldHeight);
   PP.camera.start_follow(scene, PP.game_state.player, 0, 0);
 
-  // === CAMBIO MONDO (U / u) ===
-  PP.game_state.changingWorld = false;
-  //scene.input.keyboard.on('keydown-U', () => switchWorld(scene));
-  //scene.input.keyboard.on('keydown-u', () => switchWorld(scene));
-//}
+  // === CLICK DEL MOUSE PER ATTACCARE ===
+    scene.input.on("pointerdown", () => {
+        PP.entities.player.attack(scene, PP.game_state.player, PP.game_state.boss);
+    });
 
-// === FUNZIONE CAMBIO MONDO ===
-//function switchWorld(scene) {
-  //if (PP.game_state.changingWorld) return;
-  PP.game_state.changingWorld = true;
+  // --- NEVE ---
+    // array per i fiocchi
+    scene.snowflakes = [];
 
-  // Salva posizione globale
-  //PP.game_state.playerPosition = {
-  //  x: PP.game_state.player.x,
-  //  y: PP.game_state.player.y
-  //};
+    // timer che crea fiocchi
+    scene.time.addEvent({
+        delay: 200,
+        callback: () => {
+            const x = Phaser.Math.Between(0, scene.sys.game.config.width);
+            const flake = scene.add.image(x, 0, 'snowflake').setScale(0.2);
+            scene.snowflakes.push(flake);
+        },
+        loop: true
+    });
 
-  //const currentScene = scene.scene.key;
-  //const nextScene = currentScene.startsWith('ghostly_')
-   // ? currentScene.replace('ghostly_', '')
-   // : 'ghostly_' + currentScene;
-
-  //scene.cameras.main.fadeOut(500, 0, 0, 0);
-  //scene.time.delayedCall(500, () => {
-   // const { x, y } = PP.game_state.playerPosition;
-   // scene.scene.start(nextScene, { x, y });
-   // PP.game_state.changingWorld = false;
-  //});
+    PP.game_state.changingWorld = false;
 }
 
 function update_forest(scene) {
@@ -175,6 +163,14 @@ function update_forest(scene) {
        y: PP.game_state.player.y
     };
   }
+
+   // muovi i fiocchi verso il basso
+    for (let flake of scene.snowflakes) {
+        flake.y += 2; // velocità caduta
+        if (flake.y > scene.sys.game.config.height) {
+            flake.destroy();
+        }
+    }
 }
 
 
@@ -184,6 +180,8 @@ function destroy_forest(scene) {
 
 // === AGGIUNGI LA SCENA ===
 PP.scenes.add('forest_scene', preload_forest, create_forest, update_forest, destroy_forest);
+
+
 
 
 
