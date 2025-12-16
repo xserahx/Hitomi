ad4c48a4656b5239770140ed4c131ef36b9d54f4
Serena Serafini <xxladantessaxx@gmail.com>
2025-12-16 13:29:10 +0100

    Sistemata foresta con sprite dei nemici nelle posizioni fornite
    Updated enemy positions and added enemy preload functionality.

--- hitomi_game/src/scenes/forest_scene.js
+++ hitomi_game/src/scenes/forest_scene.js
@@ -6,6 +6,7 @@ function preload_forest(scene) {
   PP.game_state.lives = PP.assets.sprite.load_spritesheet(scene, "assets/images/heart.png", 120, 50);
   PP.scene_objects.platform.preload(scene);
   PP.entities.player.preload(scene);
+  PP.entities.enemy.preload(scene);
 }
 
 function create_forest(scene, data) {
@@ -120,14 +121,14 @@ function create_forest(scene, data) {
     }
     
     // === NEMICI ===
-    const enemyPositions = [{ x: 400, y: 825, speed: 80 }, //pterodattilo
-      { x: 1200, y: 825, speed: 80 }, //slug
-      { x: 2250, y: 825, speed: 80 }, //lanterna
-      { x: 2050, y: 155, speed: 80 }, // pterodattilo
-      { x: 3150, y: 825, speed: 80 }, //lanterna
-      { x: 3350, y: 825, speed: 80 }, //pterodattilo
-        { x: 3550, y: 825, speed: 80 }, //slug
-
+    const enemyPositions = [
+      { x: 400, y: 700, w: 75, h: 75, speed: 80, sprite_name: "pterodattilo" }, 
+      { x: 1200, y: 700, w: 170, h: 170, speed: 80, sprite_name: "slug" },
+      { x: 2250, y: 700, w: 75, h: 75, speed: 80, sprite_name: "lanterna" },
+      { x: 2050, y: 155, w: 75, h:75, speed: 80, sprite_name: "pterodattilo" },
+      { x: 3150, y: 700, w: 75, h: 75, speed: 80, sprite_name: "lanterna" },
+      { x: 3350, y: 700, w: 75, h:75, speed: 80, sprite_name: "pterodattilo" },
+      { x: 3550, y: 700, w: 170, h: 170, speed: 80, sprite_name: "slug" }
     ];
     PP.game_state.enemies = PP.entities.enemy.create(scene, enemyPositions);
 
