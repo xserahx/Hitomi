b6aa5e7dce835c8fae76c3120641958962521ca5
Serena Serafini <xxladantessaxx@gmail.com>
2026-01-19 23:35:39 +0100

    Sfondo cambio scena / sfondo di default reso nero e non più blu

--- hitomi_game/src/main.js
+++ hitomi_game/src/main.js
@@ -2,7 +2,7 @@ const config = {
   canvas_width: 1280,
   canvas_height: 720,
   canvas_id: 'game',
-  background_color: 0x00007E,
+  background_color: 0x000000,
   debug_mode: true,
   gravity_value: 600,
   player_x: 0,
