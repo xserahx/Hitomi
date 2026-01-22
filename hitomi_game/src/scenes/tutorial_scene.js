9c58d87ddabf31d2d53bc2d8cb634ba8424eb122
Marcello Federici <marcello.federici@mail.polimi.it>
2026-01-22 10:58:17 +0100

    modifica tutorial

--- hitomi_game/src/scenes/tutorial_scene.js
+++ hitomi_game/src/scenes/tutorial_scene.js
@@ -28,17 +28,19 @@ function create_tutorial_scene(scene) {
 
     // === PIATTAFORME ===
     const platformPositions = [ 
-        { x: 380, y: 465, w: 40, h: 395, sprite_name: "palo" },  // colonna di sinistra
         { x: 820, y: 675, w: 150, h: 40, sprite_name: "piattaforma" },  // piattaforma iniziale
         { x: 1030, y: 767, w: 110, h: 90, sprite_name: "rialzino" }, // muretto
         { x: 400, y: 550, w: 150, h: 20, sprite_name: "basetta" }, // base del nemico
-        { x: 60, y: 797, w: 100, h: 60, sprite_name: "culla" }   // culla del bimbo
+        { x: 232, y: 640, w: 150, h: 20, sprite_name: "basetta"}, // base sopra culla
+        // /DA FLIPPARE
+        { x: 150, y: 797, w: 100, h: 60, sprite_name: "culla" },   // culla del bimbo
+        { x: 378, y: 465, w: 40, h: 395, sprite_name: "palo" }  // colonna di sinistra
     ];
 
     PP.game_state.platforms = PP.scene_objects.platform.create(scene, platformPositions);
 
     // === BAMBINO ===
-    const baby = PP.shapes.rectangle_add(scene, 95, 777, 40, 40, "0xffffff", 1);
+    const baby = PP.shapes.rectangle_add(scene, 185, 777, 40, 40, "0xffffff", 1);
     PP.physics.add(scene, baby, PP.physics.type.STATIC);
 
     // === PLAYER ===
