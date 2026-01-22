154c4195baeb4ad80d298f25242aea626d56845d
Marcello Federici <marcello.federici@mail.polimi.it>
2026-01-22 11:10:28 +0100

    corezione piattafrome ghost tutorial

--- hitomi_game/src/scenes/ghostly_tutorial_scene.js
+++ hitomi_game/src/scenes/ghostly_tutorial_scene.js
@@ -27,6 +27,7 @@ function create_ghostly_tutorial_scene(scene) {
     // === PIATTAFORME ===
     const platformPositions = [ 
         { x: 820, y: 675, w: 150, h: 40, sprite_name: "piattaforma" },  // piattaforma iniziale
+        { x: 610, y: 600, w: 150, h: 40, sprite_name: "piattaforma" },  // piattaforma iniziale
         { x: 1030, y: 767, w: 110, h: 90, sprite_name: "rialzino" }, // muretto
         { x: 400, y: 550, w: 150, h: 20, sprite_name: "basetta" }, // base del nemico
         { x: 232, y: 640, w: 150, h: 20, sprite_name: "basetta"}, // base sopra culla
