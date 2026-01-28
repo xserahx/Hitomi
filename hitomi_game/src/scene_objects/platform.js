f7686c8d3f1295913608c881a82dd5647533f13e
Serena Serafini <xxladantessaxx@gmail.com>
2026-01-28 01:37:33 +0100

    Fix piattaforme + nuove aggiunte

--- hitomi_game/src/scene_objects/platform.js
+++ hitomi_game/src/scene_objects/platform.js
@@ -5,15 +5,23 @@ PP.scene_objects.platform = {};
 PP.scene_objects.platform.preload = function(scene) {
     // carico le immagini e salvo i riferimenti 
     PP.scene_objects.platform.sprite = {};
+
+    // Piattaforme tutorial e casa
     PP.scene_objects.platform.sprite["vaso"] = PP.assets.image.load(scene, "assets/images/house/vaso.png");
     PP.scene_objects.platform.sprite["piattaforma"] = PP.assets.image.load(scene, "assets/images/house/piattaforma.png");
-    PP.scene_objects.platform.sprite["armadio"] = PP.assets.image.load(scene, "assets/images/house/armadio.png");
+     PP.scene_objects.platform.sprite["armadio"] = PP.assets.image.load(scene, "assets/images/house/armadio.png");
     PP.scene_objects.platform.sprite["rialzino"] = PP.assets.image.load(scene, "assets/images/house/rialzino.png");
     PP.scene_objects.platform.sprite["muro_grande"] = PP.assets.image.load(scene, "assets/images/house/muro_grande.png", 100, 620);
     PP.scene_objects.platform.sprite["basetta_1"] = PP.assets.image.load(scene, "assets/images/house/basetta_1.png", 150, 20);
     PP.scene_objects.platform.sprite["basetta_2"] = PP.assets.image.load(scene, "assets/images/house/basetta_2.png", 150, 20);
     PP.scene_objects.platform.sprite["palo"] = PP.assets.image.load(scene, "assets/images/house/palo.png", 50, 270);
     PP.scene_objects.platform.sprite["culla"] = PP.assets.image.load(scene, "assets/images/culla.png", 100, 60);
+
+    // Piattaforme foresta
+    PP.scene_objects.platform.sprite["tronco"] = PP.assets.image.load(scene, "assets/images/forest/parallasse/tronco.png", 300, 350);
+    PP.scene_objects.platform.sprite["piattaforma_foresta"] = PP.assets.image.load(scene, "assets/images/forest/parallasse/piattaforma_erbetta.png");
+    PP.scene_objects.platform.sprite["roccia_1"] = PP.assets.image.load(scene, "assets/images/forest/parallasse/roccia_1.png", 500, 150);
+    PP.scene_objects.platform.sprite["roccia_2"] = PP.assets.image.load(scene, "assets/images/forest/parallasse/roccia_2.png", 500, 150);
 
     // Particelle 
     let graphics = scene.add.graphics();
@@ -47,7 +55,7 @@ PP.scene_objects.platform.create = function(scene, positions) {
             lifespan: 1500,
             scale: { start: 1, end: 0.5 },
             alpha: { start: 0.8, end: 0 },
-            tint: 0xBF40BF,
+            tint: 0x93DC5C,
             blendMode: 'SCREEN',
             speedX: { min: -10, max: 10 },
             speedY: { min: -10, max: 10 },
